7260a6aef96b2e0b10077126c1667066e365c01d
Eric Butera <eric@ericbutera.com>
2022-09-02 20:11:06 +0000

    style: cleanup

--- dags/nist_cve.py
+++ dags/nist_cve.py
@@ -1,25 +1,33 @@
-import tempfile
-import datetime
-import json
+"""NIST CVE demo DAG"""
 import os
+import json
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 
+# Use
+# 1. create a connection called `google_cloud_default` for GCP in
+#    http://localhost:8080/connection/list/
+# 2. Ensure ENV variables are set in .env
+#
+# https://airflow.apache.org/docs/apache-airflow/2.2.4/best-practices.html#writing-a-dag
+#
+# Google Operator examples:
 # https://github.com/apache/airflow/tree/main/airflow/providers/google/cloud/example_dags
 #
-# TODO: Replace hard-coded paths with `airflow.models.Variable` or xcom
-# TODO: full vs incremental
-# TODO: its possible to pass config variables into the DAG, like last run date
-# TODO validation at each step, do not proceed
-# TODO gcp https://github.com/BasPH/data-pipelines-with-apache-airflow/blob/master/chapter18/dags/gcp.py
-# TODO gcp https://cloud.google.com/composer/docs/how-to/using/writing-dags
-# TODO testing https://medium.com/@jw_ng/writing-unit-tests-for-an-airflow-dag-78f738fe6bfc
-# TODO observability https://docs.datadoghq.com/integrations/airflow/?tab=host
+# - Replace hard-coded paths with `airflow.models.Variable` or xcom
+# - full vs incremental
+# - its possible to pass config variables into the DAG, like last run date
+# - validation at each step, do not proceed
+# - gcp https://cloud.google.com/composer/docs/how-to/using/writing-dags
+# - testing https://medium.com/@jw_ng/writing-unit-tests-for-an-airflow-dag-78f738fe6bfc
+# - observability https://docs.datadoghq.com/integrations/airflow/?tab=host
+# - lint https://pypi.org/project/pylint-airflow/
 #
 # Concerns
 # - DAG updating
@@ -42,9 +50,9 @@ args = {
     'owner': 'eb',
 }
 
-def _json_load(path):
-    with open(path, "r") as file:
-        return json.load(file)
+def _json_load(filename):
+    with open(filename) as f:
+        return json.load(f)
 
 gcp_project_id = os.environ.get('NISTY_GCP_PROJECT')
 gcp_data_set = os.environ.get('NISTY_GCP_DATA_SET')
@@ -53,10 +61,10 @@ table_id = f"{gcp_project_id}.{gcp_data_set}.cves"
 table_schema = _json_load("/schemas/nist_cve.json")
 
 def _transform(**context):
-    # TODO: fix data_path
-    # TODO: harden against errors
-    # TODO: stream, not bulk load
-    # TODO: run in resource constrained env
+    # - fix data_path
+    # - harden against errors
+    # - stream, not bulk load
+    # - run in resource constrained env
     context['data_path'] = '/data'
     path = context["data_path"] + "/" + context["run_id"] + "/raw.json"
     data = _json_load(path)
@@ -65,7 +73,8 @@ def _transform(**context):
     with open(out_path, "w") as out:
         for cve in data["result"]["CVE_Items"]:
             row = json.dumps(cve)
-            out.write(row + "\n")
+            out.write(row + "\n") #
+
 with DAG(
     dag_id='nist_cve',
     default_args=args,
@@ -80,69 +89,65 @@ with DAG(
 ) as dag:
     prepare = BashOperator(
         task_id='prepare',
-        bash_command="mkdir -p {{ data_path }}/{{run_id}}"
+        bash_command="mkdir -p {{ data_path }}/{{run_id}}",
     )
+    # prepare = DummyOperator(task_id='prepare')
 
     # get API data, save locally
-    # /data/runid/raw.json
-    # Note: add a TTL on these buckets that expire much quicker than the later transformations
-    # This step will be a cost bottleneck.
-    # Each transform becomes more valuable; find ways to lower cost using things like S3 glacier, bucket TTL/expiry, compression
+    # Note: add a TTL on these buckets that expire much quicker than the later transformations (save $)
+    # Each transform becomes more valuable; use cost-saving measures like S3 glacier, bucket TTL/expiry, compression
     extract = BashOperator(
         task_id='extract',
         bash_command='curl {{ nist_cve_endpoint }} --output {{ data_path }}/{{ run_id }}/raw.json',
     )
+    # extract = DummyOperator(task_id='extract')
 
     # basically this copies the local API response into the data lake
     # if any of the downstream steps fail, its important to have this source data to retry
-    # Note: use storage exiry and governance
     store_raw = LocalFilesystemToGCSOperator(
         task_id="store_raw",
         src="{{ data_path }}/{{ run_id }}/raw.json",
         dst="{{ run_id }}/raw.json",
         bucket="{{ bucket }}",
     )
+    # store_raw = DummyOperator(task_id="store_raw")
 
-    # GoogleCloudStorageToGoogleCloudStorageOperator
-    # TODO: convert to jsonlines, avro, etc
-    # TODO: jq '.result.CVE_Items[].cve.CVE_data_meta.ID'
-    #   Note: jq supports streaming!
-    # TODO: alternative: https://avro.apache.org/docs/1.10.2/gettingstartedpython.html
-    # TODO: Move this to a cluster with explicit resource limits.
-    #   PythonOperators are a bad idea, can easily lead to OOM situations
+    # Transform from raw into JSONLines (required for BigQuery load)
+    # Alternatives:
+    # - (streaming) jq '.result.CVE_Items[].cve.CVE_data_meta.ID'
+    # - avro https://avro.apache.org/docs/1.10.2/gettingstartedpython.html
+    #
+    # - Move this to a cluster with explicit resource limits.
+    # - PythonOperators are a bad idea, can easily lead to OOM situations
     transform = PythonOperator(
         task_id='transform',
         python_callable=_transform,
-        dag=dag
+        dag=dag,
     )
+    # transform = DummyOperator(task_id='transform')
 
     store_transform = LocalFilesystemToGCSOperator(
         task_id="store_transform",
-        # src="{{ data_path }}/{{ run_id }}.json",
         src="{{ data_path }}/{{ run_id }}/data.jsonlines",
         dst="{{ run_id }}/lines.jsonlines",
         bucket="{{ bucket }}",
     )
+    # store_transform = DummyOperator(task_id="store_transform")
 
     # Generate schema with:
     # bq show --schema --format=prettyjson myprojectid:mydataset.mytable > schemas/nist_cve.json
     load = GoogleCloudStorageToBigQueryOperator(
-        task_id                             = "datalake_to_bigquery",
+        task_id                             = "load",
         bucket                              = "{{ bucket }}",
         source_objects                      = ["{{ run_id }}/*.jsonlines"],
         destination_project_dataset_table   = table_id,
         source_format                       = "NEWLINE_DELIMITED_JSON",
         write_disposition                   = 'WRITE_TRUNCATE',
-        # autodetect                        = True,
         schema_fields                       = table_schema,
     )
+    # load = DummyOperator(task_id="load")
 
-    prepare >> \
-    extract >> \
-    store_raw >> \
-    transform >> \
-    store_transform >> \
-    load
+    prepare >> extract >> store_raw >> transform >> store_transform >> load
 
 if __name__ == "__main__":
     dag.cli()
